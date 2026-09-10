606343c1428f37c07866f4b11d4007ec82765513
mofomo55 <moh55eibou@gmail.com>
2026-09-10 18:26:13 +0200

    Created the city-accordion Element and added it in myListing page to show Cties and companies

--- ClientSide/CoopRadarClient/src/app/features/listing/pages/my-listings/my-listings.ts
+++ ClientSide/CoopRadarClient/src/app/features/listing/pages/my-listings/my-listings.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CityAccordionComponent } from '../../../../shared/components/city-accordion/city-accordion';
+import { MyListingsService } from '../../services/my-listings.service';
 
 @Component({
   selector: 'app-my-listings-page',
   standalone: true,
+  imports: [CommonModule, CityAccordionComponent],
   templateUrl: './my-listings.html',
   styleUrl: './my-listings.css'
 })
-export class MyListingsPage {}
+export class MyListingsPage {
+  private readonly myListingsService = inject(MyListingsService);
+
+  readonly cityGroups = this.myListingsService.cityGroups;
+
+  onCompanySelected(company: { name: string }): void {
+    console.log('Selected company:', company.name);
+  }
+}
